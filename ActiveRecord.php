d7364e82fcbe253527fa2f52a7e8a80a763ec19d
lloydzhou <lloydzhou@qq.com>
2016-04-02 02:55:25 +0800

    update doc for single parameter

--- ActiveRecord.php
+++ ActiveRecord.php
@@ -181,6 +181,7 @@ abstract class ActiveRecord extends Base {
      * @param string $sql The SQL to find record.
      * @param array $param The param will be bind to PDOStatement.
      * @param ActiveRecord $obj The object, if find record in database, will assign the attributes in to this object.
+     * @param bool $single if set to true, will find record and fetch in current object, otherwise will find all records.
      * @return bool|ActiveRecord|array
      */
     public static function _query($sql, $param = array(), $obj = null, $single=false) {
