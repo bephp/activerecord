be9ff04f02ebfca1e9932f44f079fd96b6eb4902
lloydzhou <lloydzhou@qq.com>
2017-05-27 19:04:56 +0800

    hotfix for phpunit

--- test/ActiveRecordTest.php
+++ test/ActiveRecordTest.php
@@ -1,5 +1,9 @@
 <?php 
 
+if (!class_exists('PHPUnit_Framework_TestCase')) {
+    class PHPUnit_Framework_TestCase extends \PHPUnit\Framework\TestCase {}
+}
+
 class User extends ActiveRecord{
     public $table = 'user';
     public $primaryKey = 'id';
